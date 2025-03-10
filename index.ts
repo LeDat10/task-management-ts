f6bc7dd584e71b0a9582c003a36f4b1a55666ef6
ledat <ledat1010204@gmail.com>
2025-03-10 14:31:08 +0700

    Tạo Routes

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Oke");
+import express, {Express} from "express";
+import * as database from "./config/database";
+import dotenv from "dotenv";
+
+import mainV1Routes from "./api/v1/routes/index.route";
+
+dotenv.config();
+
+database.connect();
+
+const app: Express = express();
+const port: number | string = process.env.PORT;
+
+
+mainV1Routes(app);
+
+app.listen(port, () => {
+    console.log(`App listen on port ${port}`);
+});
